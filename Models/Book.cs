d9b46796d6fe3a8fd75283c015cd8ef5264015d5
giangtiny <giangtiny12@gmail.com>
2022-04-06 10:42:40 +0700

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Book_Exchange_Website.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string BookName { get; set; }
+        public string BookImage { get; set; }
+        public string BookTitle { get; set; }
+        public string BookAuthor { get; set; }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Book_Exchange_Website.Startup))]
+namespace Book_Exchange_Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
